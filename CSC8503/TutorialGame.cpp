556fbae61f428bff21e6a1001995a16d4b169ee8
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-11-24 14:35:20 +0000

    Add extra weight to spheres

--- CSC8503/TutorialGame.cpp
+++ CSC8503/TutorialGame.cpp
@@ -381,9 +381,9 @@ void TutorialGame::CreatedMixedGrid(int numRows, int numCols, float rowSpacing, 
 			}
 			else {
 				if (rand() % 3 == 0) {
-					AddSphereToWorld(position, sphereRadius, 1.0f, true);
+					AddSphereToWorld(position, sphereRadius, 5.0f, true);
 				}
-				AddSphereToWorld(position, sphereRadius, 1.0f);
+				AddSphereToWorld(position, sphereRadius, 5.0f);
 			}
 		}
 	}
