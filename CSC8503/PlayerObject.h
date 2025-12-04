b1d45d61c36fcadce09c682ad15e9cff460b0fb4
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-04 17:23:41 +0000

    Feat: Implement basic movement including jumping with cooldowns

---
+++ CSC8503/PlayerObject.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "GameObject.h"
+#include "Camera.h"
+
+using namespace NCL::Maths;
+
+class PlayerObject : public NCL::CSC8503::GameObject
+{
+public:
+	PlayerObject() : GameObject("PlayerObject")
+	{
+	}
+
+	~PlayerObject()
+	{
+	}
+
+	void Update(float dt) override;
+
+	void OnCollisionBegin(NCL::CSC8503::GameObject* otherObject) override {
+		canJump = true;
+	}
+
+	float GetJumpCooldown() const {
+		return jumpCooldown;
+	}
+	void SetJumpCooldown(float cooldown) {
+		jumpCooldown = cooldown;
+		canJump = false;
+	}
+	bool CanJump() const {
+		return canJump&&(jumpCooldown<=0.0f);
+	}
+
+protected:
+	float jumpCooldown = 0.0f;
+	bool canJump = true;
+};
