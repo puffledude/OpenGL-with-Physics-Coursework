9fca131a52e5af5ee7a967afc27b5bca1b96269f
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-08 15:39:28 +0000

    Feat: Add inital files for punchboxes

---
+++ CSC8503/PunchBox.cpp
@@ -0,0 +1,8 @@
+#include "PunchBox.h"
+
+void PunchBox::Update(float dt) {
+	/*Punches in direction with force each frame.
+	* When at max distance, invert direction and reverse.
+	* Maybe kill momentum when changing direction?
+	*/
+}
