eeb1704da8ddc2491f057308b5e30a7714faaa78
puffledude <jammyjoe10@gmail.com>
2025-12-15 00:30:17 +0000

    Add goose attack method and transition state

--- CSC8503/Goose.cpp
+++ CSC8503/Goose.cpp
@@ -1,31 +1,38 @@
 #include "Goose.h"
 #include "State.h"
+#include "PhysicsObject.h"
 
-Goose::Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh)
-	: EnemyAI(waypoints, areaMesh) {
+Goose::Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, GameObject* playerObject)
+	: EnemyAI(waypoints, areaMesh, playerObject) {
 	
 	patrolState = new NCL::CSC8503::State([&](float dt)-> void {
 		Patrol(dt);
 		});
 
 	stateMachine.AddState(patrolState);
-	/*attackState = new NCL::CSC8503::State([&](float dt)-> void {
+	attackState = new NCL::CSC8503::State([&](float dt)-> void {
 		attackPlayer(dt);
-		});*/
-	//stateMachine.AddState(attackState);
-	//patrolToAttack = new NCL::CSC8503::StateTransition(patrolState, attackState, [&]()-> bool {
-	//	//Check if player is in sight
-	//	return canSeePlayer();
-	//	});
-	//stateMachine.AddTransition(patrolToAttack);
-	//attackToPatrol = new NCL::CSC8503::StateTransition(attackState, patrolState, [&]()-> bool {
-	//	//Check if player is no longer in sight
-	//	return !canSeePlayer();
-	//	});
+		});
+	stateMachine.AddState(attackState);
+	patrolToAttack = new NCL::CSC8503::StateTransition(patrolState, attackState, [&]()-> bool {
+		//Check if player is in sight
+		return canSeePlayer();
+		});
+	stateMachine.AddTransition(patrolToAttack);
+	attackToPatrol = new NCL::CSC8503::StateTransition(attackState, patrolState, [&]()-> bool {
+		//Check if player is no longer in sight
+		return !canSeePlayer();
+		});
 	//stateMachine.AddTransition(attackToPatrol);
 }
 
 void Goose::Update(float dt) {
 	stateMachine.Update(dt);
 }
+
+void Goose::attackPlayer(float dt) {
+	{
+		Vector3 direction = Vector::Normalise(playerObject->GetTransform().GetPosition() - this->GetTransform().GetPosition());
+		this->GetPhysicsObject()->AddForce(direction * moveSpeed * dt);
+	}
 
--- CSC8503/Goose.h
+++ CSC8503/Goose.h
@@ -4,7 +4,7 @@
 class Goose : public EnemyAI 
 {
 public:
-	Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh);
+	Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, GameObject* playerObject);
 
 	void Update(float dt) override;
 protected:
