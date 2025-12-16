d20309f566f00cf48c02d5d012418afffdbe7b41
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-16 14:34:00 +0000

    Reorganise main menu

--- CSC8503/GameMainMenu.cpp
+++ CSC8503/GameMainMenu.cpp
@@ -3,8 +3,8 @@
 #include "InGame.h"
 
 NCL::CSC8503::PushdownState::PushdownResult GameMainMenu::OnUpdate(float dt, NCL::CSC8503::PushdownState** pushFunc){
-	NCL::Debug::Print("Glass Run:", NCL::Vector2(50,50));
-	NCL::Debug::Print("Press ENTER to Start", NCL::Vector2(50, 40));
+	NCL::Debug::Print("Glass Run", NCL::Vector2(50, 40));
+	NCL::Debug::Print("Press ENTER to Start", NCL::Vector2(50, 50));
 	if (NCL::Window::GetKeyboard()->KeyPressed(NCL::KeyCodes::ESCAPE)) {
 		return NCL::CSC8503::PushdownState::Pop;
 	}
