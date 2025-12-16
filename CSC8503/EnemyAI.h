a33adc2eb9fdc37e8556f0b2c734fbe22d28d902
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-16 22:50:02 +0000

    Enemy AI now take a reference to the vector of all players

--- CSC8503/EnemyAI.h
+++ CSC8503/EnemyAI.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GameObject.h"
+#include "PlayerObject.h"
 #include "Vector.h"
 #include "StateMachine.h"
 #include "NavigationGrid.h"
@@ -12,7 +13,7 @@ public:
 	//Could use pushdown Automata here?
 	//Patrol until player seen, then chase player.
 	//Once player is no longer visable, return to patrol.
-	EnemyAI(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, GameObject* playerObject);
+	EnemyAI(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, std::vector<PlayerObject*>& playerObjects);
 	void Update(float dt) override;
 
 	float getMoveSpeed() const {
@@ -24,11 +25,11 @@ public:
 
 protected:
 	NCL::CSC8503::StateMachine stateMachine{};
-	GameObject* playerObject = nullptr;
+	std::vector<PlayerObject*>& players;
 	void Patrol(float dt);
-	bool canSeePlayer();
+	int canSeePlayer();
 	float fovAngle = 150.0f; //Degrees
-	float sightDistance = 60.0f;
+	float sightDistance = 100.0f;
 	std::vector<Vector3> patrolWaypoints;
 	int targetWaypointIndex = 0;
 	Vector3 targetWaypoint;
