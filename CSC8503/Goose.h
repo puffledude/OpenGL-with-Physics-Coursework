819976bbe426b4c5d1f253636cc87f8120e843fb
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-16 22:50:19 +0000

    Update constructor to take reference to list of all players

--- CSC8503/Goose.h
+++ CSC8503/Goose.h
@@ -5,13 +5,15 @@
 class Goose : public EnemyAI 
 {
 public:
-	Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, GameObject* playerObject);
+	Goose(std::vector<NCL::Vector3> waypoints, NCL::CSC8503::NavigationMesh* areaMesh, std::vector<PlayerObject*>& PlayerObjects);
 
 	void Update(float dt) override;
 
 	void OnCollisionBegin(GameObject* otherObject) override;
 protected:
 	void attackPlayer(float dt);
+	int targetPlayer = -1;
+	float sinceLastSighting = 0.0f;
 	NCL::CSC8503::State* patrolState;
 	NCL::CSC8503::State* attackState;
 	NCL::CSC8503::StateTransition* patrolToAttack;
