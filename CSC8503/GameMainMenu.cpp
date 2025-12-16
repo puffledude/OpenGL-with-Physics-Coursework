f80717e41ff08770fd52979e3db344460e0622c1
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2025-12-16 19:18:32 +0000

    Give options to open a hosted game

--- CSC8503/GameMainMenu.cpp
+++ CSC8503/GameMainMenu.cpp
@@ -1,18 +1,29 @@
 #include "GameMainMenu.h"
 #include "PushdownState.h"
 #include "TutorialGame.h"
+#include "NetworkedGame.h"
 #include "InGame.h"
+#include "InGameServer.h"
+#include "InGameClient.h"
 
 
 NCL::CSC8503::PushdownState::PushdownResult GameMainMenu::OnUpdate(float dt, NCL::CSC8503::PushdownState** pushFunc){
 	NCL::Debug::Print("Glass Run", NCL::Vector2(50, 40));
 	NCL::Debug::Print("Press ENTER to Start", NCL::Vector2(50, 50));
+	NCL::Debug::Print("Press H to Host a server", NCL::Vector2(50, 60));
+	NCL::Debug::Print("Press J to Join a server", NCL::Vector2(50, 70));
 	if (NCL::Window::GetKeyboard()->KeyPressed(NCL::KeyCodes::ESCAPE)) {
 		return NCL::CSC8503::PushdownState::Pop;
 	}
 	if (NCL::Window::GetKeyboard()->KeyPressed(NCL::KeyCodes::RETURN)) {
 		NCL::CSC8503::TutorialGame* tutorialGame = new TutorialGame(*gameWorld, *renderer, *physics);
 		*pushFunc = new InGame(gameWorld, physics, renderer, tutorialGame, menuWidth, menuHeight);
+		return NCL::CSC8503::PushdownState::Push;
+	}
+	if (NCL::Window::GetKeyboard()->KeyPressed(NCL::KeyCodes::H)) {
+		NCL::CSC8503::NetworkedGame* networkedGame = new NCL::CSC8503::NetworkedGame(*gameWorld, *renderer, *physics);
+		networkedGame->StartAsServer();
+		*pushFunc = new InGameServer(gameWorld, physics, renderer, networkedGame, menuWidth, menuHeight);
 		return NCL::CSC8503::PushdownState::Push;
 	}
 
---
+++ CSC8503/InGameServer.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "GamePause.h"
+#include "pushdownState.h"
+#include "TutorialGame.h"
+#include "GameTechRenderer.h"
+#include "PhysicsSystem.h"
+
+class InGameServer : public NCL::CSC8503::PushdownState
+{
+public:
+	InGameServer(NCL::CSC8503::GameWorld* world, NCL::CSC8503::PhysicsSystem* physics, NCL::CSC8503::GameTechRenderer* renderer,
+		NCL::CSC8503::NetworkedGame* networkedgame, float width, float height) :
+		gameWorld(world), physics(physics), renderer(renderer), networkedGame(networkedgame), width(width), height(height) {
+	}
+
+	PushdownResult OnUpdate(float dt, PushdownState** pushFunc) override {
+		if (NCL::Window::GetKeyboard()->KeyPressed(NCL::KeyCodes::P)) {
+			*pushFunc = new GamePause();
+			return PushdownResult::Push;
+		}
+		networkedGame->UpdateGame(dt);
+		gameWorld->UpdateWorld(dt);
+		physics->Update(dt);
+		//renderer->Update(dt);
+		return PushdownResult::NoChange;
+	}
+
+
+protected:
+	float width;
+	float height;
+	NCL::CSC8503::GameWorld* gameWorld;
+	NCL::CSC8503::PhysicsSystem* physics;
+	NCL::CSC8503::GameTechRenderer* renderer;
+	NCL::CSC8503::NetworkedGame* networkedGame;
+};
